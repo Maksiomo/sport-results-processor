6dfb2362b6d50b6389a95d57a9040423eca4772b
Maxiomo <golanov.m@mascot.games>
2025-04-28 17:00:39 +0400

    wip

--- internal/app/sport_registry_server.go
+++ internal/app/sport_registry_server.go
@@ -1,1 +1,17 @@
 package app
+
+import (
+	"context"
+	"sport-results-pocessor/internal/common/adapter/config"
+	"sport-results-pocessor/internal/common/adapter/logger"
+
+	"go.uber.org/fx"
+)
+
+func SportRegistryServer(
+	lf fx.Lifecycle,
+	ctx context.Context,
+	cfg *config.Config,
+	log logger.Logger,
+	sportR 
+)
